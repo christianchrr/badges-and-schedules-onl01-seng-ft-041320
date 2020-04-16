14c2256dde1744c0e53024f703c2cd1c84a2cc56
Christian Cherry <christianchrr@gmail.com>
2020-04-16 18:46:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,6 +19,11 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  
+  batch_badge_creator(attendees).each do |x|
+    puts x
+  end
+  assign_rooms(attendees).each do |y|
+    puts y
+  end
 end
     
